94bdb6991187415d94cc9afae368379e89487b07
Mirko R <mirko_richter@me.com>
2022-12-18 10:52:32 +0100

    reconfig for local development

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
 
-//const API_URL = 'http://127.0.0.1:5000/api/v1/tasks'
-const API_URL = 'http://192.168.0.29:5000/api/v1/tasks'
+const API_URL = 'http://127.0.0.1:5000/api/v1/tasks'
+//const API_URL = 'http://192.168.0.29:5000/api/v1/tasks'
 
 const ADD_TASK = 'ADD_TASK'
 const SET_TASKS = 'SET_TASKS'
