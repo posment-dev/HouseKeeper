328cfbc7c705af0807f3f230a3a1ed40a2dd0b6f
Mirko R <mirko_richter@me.com>
2023-03-04 08:59:30 +0100

    new db table for diet entries added

--- api/constants.py
+++ api/constants.py
@@ -1,2 +1,3 @@
 DB_LOCATION = 'sqlite:///db/tasks.db?check_same_thread=False'
 DB_BUDGET_LOCATION = 'sqlite:///db/budget_entries.db?check_same_thread=False'
+DB_DIET_LOCATION = 'sqlite:///db/diet.db?check_same_thread=False'
